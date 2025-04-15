e3bdbdf9258a46480e789ef4e249267d6f2080b5
Ezatullah Farazi <ezat.farazi@gmail.com>
2025-04-14 22:19:12 -0400

    asking user for text and phone_num

---
+++ main.py
@@ -0,0 +1,8 @@
+import requests
+
+# The scripts asks for text and phone number.
+# The input text will be sent to input phone number
+
+user_text = input("Please, input the text you want to send: ")
+user_phone_number = input("Please, insert the phone number you want to send text to: ")
+
